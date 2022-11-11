47995d46ef5ebf75d23955426214738c91af7016
Мария <glsip.girl1995@gmail.com>
2022-11-11 16:16:03 +0300

    Добавляет форму и контакты

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -1,10 +1,20 @@
+// import {FocusLock} from '../utils/focus-lock';
+// import {ScrollLock} from '../utils/scroll-lock';
+
 const navElement = document.querySelector('[header-nav]');
-const headerElement = document.querySelector('[header-wrapper]');
+const headerWrapperElement = document.querySelector('[header-wrapper]');
 const logoElement = document.querySelector('[header-logo]');
 const toggleElement = document.querySelector('[header-toggle]');
+const navLinkElements = document.querySelectorAll('[header-nav-link]');
+const mainElement = document.querySelector('main');
+const headerElement = document.querySelector('[header]');
 
 if (headerElement) {
   headerElement.classList.remove('is-nojs');
+}
+
+if (headerWrapperElement) {
+  headerWrapperElement.classList.remove('is-nojs');
 }
 
 if (navElement) {
@@ -18,11 +28,21 @@ if (logoElement) {
 const openMenu = () => {
   navElement.classList.toggle('is-opened');
   navElement.classList.toggle('is-closed');
-  headerElement.classList.toggle('is-opened');
+  headerWrapperElement.classList.toggle('is-opened');
   logoElement.classList.toggle('is-opened');
 };
 
-if (headerElement && navElement) {
-  toggleElement.addEventListener('click', openMenu);
-}
+const closeMenu = () => {
+  navElement.classList.remove('is-opened');
+  navElement.classList.add('is-closed');
+  headerWrapperElement.classList.remove('is-opened');
+  logoElement.classList.remove('is-opened');
+};
 
+if (headerWrapperElement && navElement) {
+  toggleElement.addEventListener('click', openMenu);
+  for (let navLinkElement of navLinkElements) {
+    navLinkElement.addEventListener('click', closeMenu);
+  }
+  mainElement.addEventListener('click', closeMenu);
+}
